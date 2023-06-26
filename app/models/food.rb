44da3963dc7e8d4361471f1fc0f861c1ffe37df2
Nahom <zerihunnahom@gmail.com>
2023-06-26 18:02:57 +0300

    Add validations to food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,6 @@
 class Food < ApplicationRecord
     belongs_to :user
+    validates :name, :measurement_unit, :price, :quantity, presence: true
+    validates :price, :quantity, numericality: { greater_than_or_equal_to: 0 }
+    validates :quantity, numericality: {only_integer: true}
 end
