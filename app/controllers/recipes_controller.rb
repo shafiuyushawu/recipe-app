300de38255caa413d50ebeed8675ee93155c94be
Nahom <zerihunnahom@gmail.com>
2023-06-27 17:19:50 +0300

    Add create action for recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,4 +10,20 @@ class RecipesController < ApplicationController
     def new
         @recipe = Recipe.new
     end
+
+    def create
+        @recipe = current_user.recipes.new(recipe_params)
+
+        if @recipe.save
+            redirect_to recipes_path, notice: 'Recipe created successfully'
+        else
+            flash.now[:alert] = 'Unable to create a recipe'
+            render :new
+        end
+    end
+
+    private
+    def recipe_params
+        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
 end
