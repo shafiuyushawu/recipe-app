2501a11a464ed7815a12eb4e0d69dbd37c4b958a
Nahom <zerihunnahom@gmail.com>
2023-06-27 14:34:54 +0300

    Add show action for recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,8 @@ class RecipesController < ApplicationController
     def index
         @recipes = current_user.recipes.all
     end
+
+    def show
+        @recipe = Recipe.find(params[:id])
+    end
 end
