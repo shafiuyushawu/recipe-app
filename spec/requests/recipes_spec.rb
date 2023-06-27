7412cd1855644fd088179fd090a414f8a08217e3
Nahom <zerihunnahom@gmail.com>
2023-06-27 17:19:13 +0300

    Add test for the create action of recipes controller

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "Recipes", type: :request do
   let!(:chef) { User.create(name: 'Tom') }
   let!(:valid_attributes) { { name: 'recipe 1', preparation_time: 40, cooking_time: 140,
+    description: 'it is the most amazing recipe on the planet', public: true, user: chef } } 
+    
+  let!(:invalid_attributes) { { name: '', preparation_time: nil, cooking_time: nil,
     description: 'it is the most amazing recipe on the planet', public: true, user: chef } }
+
   describe "GET /index" do
     it 'returns a successful response' do
       get recipes_url
@@ -37,6 +41,24 @@ RSpec.describe "Recipes", type: :request do
     it 'renders a correct template' do
       get new_recipe_url
       expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with valid attributes' do
+      it 'creates a new recipe' do
+        expect { 
+          post recipes_path, params: {recipe: valid_attributes}
+        }.to change(Recipe, :count).by(1)
+        expect(flash[:notice]).to eq('Recipe created successfully')
+      end
+    end
+    context 'with invalid attributes' do
+      it 'creates a new recipe' do
+        post recipes_path, params: {recipe: invalid_attributes}
+        expect(response).to render_template(:new)
+        expect(flash[:alert]).to eq('Unable to create a recipe')
+      end
     end
   end
 end
