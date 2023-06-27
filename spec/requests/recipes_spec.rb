20d59bb4a9d454ed438a40419bddbb349f9630fb
Nahom <zerihunnahom@gmail.com>
2023-06-27 14:14:40 +0300

    Add test for the index action of recipes controller

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
   describe "GET /index" do
- 
+    it 'returns a successful response' do
+      get recipes_url
+      expect(response).to be_successful
+    end
+    
+    it 'renders a correct template' do
+      get recipes_url
+      expect(response).to render_template('index')
+    end
+
   end
 end
