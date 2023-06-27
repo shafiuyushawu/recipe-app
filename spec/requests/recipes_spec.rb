e6e7e6762a2cff255fc5909d9993bd931f03d2ee
Nahom <zerihunnahom@gmail.com>
2023-06-27 14:34:20 +0300

    Added test for the show action of recipes controller

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Recipes", type: :request do
+  let!(:chef) { User.create(name: 'Tom') }
+  let!(:valid_attributes) { { name: 'recipe 1', preparation_time: 40, cooking_time: 140,
+    description: 'it is the most amazing recipe on the planet', public: true, user: chef } }
   describe "GET /index" do
     it 'returns a successful response' do
       get recipes_url
@@ -11,6 +14,18 @@ RSpec.describe "Recipes", type: :request do
       get recipes_url
       expect(response).to render_template('index')
     end
+  end
+  describe 'GET /show' do
+    it 'renders a successful response' do
+      recipe = Recipe.create! valid_attributes
+      get recipe_url(recipe)
+      expect(response).to be_successful
+    end
 
+    it 'renders a correct template' do
+      recipe = Recipe.create! valid_attributes
+      get recipe_url(recipe)
+      expect(response).to render_template('show')
+    end
   end
 end
