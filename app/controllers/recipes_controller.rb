7663cd80efaef2695472c90039377fb130235d0d
Nahom <zerihunnahom@gmail.com>
2023-06-28 12:17:55 +0300

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,35 +1,36 @@
 class RecipesController < ApplicationController
-    def index
-        @recipes = current_user.recipes.all
-    end
+  def index
+    @recipes = current_user.recipes.all
+  end
 
-    def show
-        @recipe = Recipe.find(params[:id])
-    end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-    def new
-        @recipe = Recipe.new
-    end
+  def new
+    @recipe = Recipe.new
+  end
 
-    def create
-        @recipe = current_user.recipes.new(recipe_params)
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
 
-        if @recipe.save
-            redirect_to recipes_path, notice: 'Recipe created successfully'
-        else
-            flash.now[:alert] = 'Unable to create a recipe'
-            render :new
-        end
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe created successfully'
+    else
+      flash.now[:alert] = 'Unable to create a recipe'
+      render :new
     end
+  end
 
-    def destroy
-        @recipe = Recipe.find(params[:id])
-        @recipe.destroy
-        redirect_to recipes_path, notice: 'Recipe successfully deleted'
-    end
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Recipe successfully deleted'
+  end
 
-    private
-    def recipe_params
-        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-    end
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "Recipes", type: :request do
+RSpec.describe 'Recipes', type: :request do
   let!(:chef) { User.create(name: 'Tom') }
-  let!(:valid_attributes) { { name: 'recipe 1', preparation_time: 40, cooking_time: 140,
-    description: 'it is the most amazing recipe on the planet', public: true, user: chef } } 
-    
-  let!(:invalid_attributes) { { name: '', preparation_time: nil, cooking_time: nil,
-    description: 'it is the most amazing recipe on the planet', public: true, user: chef } }
+  let!(:valid_attributes) do
+    { name: 'recipe 1', preparation_time: 40, cooking_time: 140,
+      description: 'it is the most amazing recipe on the planet', public: true, user: chef }
+  end
 
-  describe "GET /index" do
+  let!(:invalid_attributes) do
+    { name: '', preparation_time: nil, cooking_time: nil,
+      description: 'it is the most amazing recipe on the planet', public: true, user: chef }
+  end
+
+  describe 'GET /index' do
     it 'returns a successful response' do
       get recipes_url
       expect(response).to be_successful
     end
-    
+
     it 'renders a correct template' do
       get recipes_url
       expect(response).to render_template('index')
@@ -49,15 +53,15 @@ RSpec.describe "Recipes", type: :request do
   describe 'POST /create' do
     context 'with valid attributes' do
       it 'creates a new recipe' do
-        expect { 
-          post recipes_path, params: {recipe: valid_attributes}
-        }.to change(Recipe, :count).by(1)
+        expect do
+          post recipes_path, params: { recipe: valid_attributes }
+        end.to change(Recipe, :count).by(1)
         expect(flash[:notice]).to eq('Recipe created successfully')
       end
     end
     context 'with invalid attributes' do
       it 'creates a new recipe' do
-        post recipes_path, params: {recipe: invalid_attributes}
+        post recipes_path, params: { recipe: invalid_attributes }
         expect(response).to render_template(:new)
         expect(flash[:alert]).to eq('Unable to create a recipe')
       end
@@ -67,11 +71,10 @@ RSpec.describe "Recipes", type: :request do
   describe 'DELETE /destroy' do
     it 'delete a recipe' do
       recipe = Recipe.create! valid_attributes
-      expect { 
+      expect do
         delete recipe_url(recipe)
-      }.to change(Recipe, :count).by(-1)
+      end.to change(Recipe, :count).by(-1)
       expect(flash[:notice]).to eq('Recipe successfully deleted')
     end
   end
-
 end
