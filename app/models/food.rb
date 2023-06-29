2b37fef00054bc6fdd04afe5dcfa151ddea0c65e
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-06-29 08:56:44 +0000

    Added dependant to destroy

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
   validates :name, :measurement_unit, :price, :quantity, presence: true
   validates :price, :quantity, numericality: { greater_than_or_equal_to: 0 }
